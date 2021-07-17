4db5e3dc5ca4b5f9b6a08d5e85db6c052032ff60
khoirul02 <hudak006@gmail.com>
2021-07-17 10:39:14 +0700

    typo css class like

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -88,7 +88,7 @@ const createLikeButtonTemplate = () => `
 `;
 
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this restaurant" id="likeButton" class="unlike">
+  <button aria-label="unlike this restaurant" id="likeButton" class="like">
     <i class="fa fa-heart" aria-hidden="true"></i>
   </button>
 `;
